df5b225f5bbd884953e93bfa65679049e34eab3e
Ahmed Elmalt <eknowledger@gmail.com>
2019-06-23 01:04:14 -0700

    added bootstrap tools; added quick build scripts

--- BuildScriptLabs/Properties/VersionInfo.cs
+++ BuildScriptLabs/Properties/VersionInfo.cs
@@ -1,5 +1,5 @@
 ﻿// Generated file - do not modify
 using System.Reflection;
 [assembly: AssemblyVersion("0.1.2")]
-[assembly: AssemblyFileVersion("0.1.2.7")]
-[assembly: AssemblyInformationalVersion("0.1.2.7.local-master-67a8264")]
+[assembly: AssemblyFileVersion("0.1.2.8")]
+[assembly: AssemblyInformationalVersion("0.1.2.8.local-master-494b77d")]
--- run-build.ps1
+++
@@ -1,3 +0,0 @@
-$scriptPath = Split-Path $MyInvocation.InvocationName
-Import-Module (join-path $scriptPath psake.psm1)
-invoke-psake -framework '4.0'
