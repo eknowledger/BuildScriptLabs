60c93e6f3173fe41d25e151a5f6fe64c9e0e2005
Ahmed Elmalt <eknowledger@gmail.com>
2019-07-04 22:24:32 -0700

    update version

--- BuildScriptLabs.Tests/Properties/VersionInfo.cs
+++ BuildScriptLabs.Tests/Properties/VersionInfo.cs
@@ -1,5 +1,5 @@
 ﻿// Generated file - do not modify
 using System.Reflection;
-[assembly: AssemblyVersion("0.1.2")]
-[assembly: AssemblyFileVersion("0.1.2.9")]
-[assembly: AssemblyInformationalVersion("0.1.2.9.local-master-df5b225")]
+[assembly: AssemblyVersion("0.1.9")]
+[assembly: AssemblyFileVersion("0.1.9.24")]
+[assembly: AssemblyInformationalVersion("0.1.9.24.local-origin/mas-20fab23")]
